fb0c6cecf62fc519b5101479323e057b1e8d17ad
Itenyo <kevinmmasi3@gmail.com>
2022-05-17 09:53:41 -0700

    my commit

--- builtin.c
+++ builtin.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "main.h"
 int (*get_builtin(char *command))(char **args, char **front);
 int shellby_exit(char **args, char **front);
 int shellby_cd(char **args, char __attribute__((__unused__)) **front);
@@ -171,17 +171,17 @@ int shellby_help(char **args, char __attribute__((__unused__)) **front)
 {
 	if (!args[0])
 		help_all();
-	else if (_strcmp(args[0], "alias") == 0)
-		help_alias();
+OAOAOA	else if (_strcmp(args[0], "alias") == 0)
+OAOAOA		help_alias();
 	else if (_strcmp(args[0], "cd") == 0)
-		help_cd();
+OAOAOA		help_cd();
 	else if (_strcmp(args[0], "exit") == 0)
-		help_exit();
+OAOAOA		help_exit();
 	else if (_strcmp(args[0], "env") == 0)
 		help_env();
-	else if (_strcmp(args[0], "setenv") == 0)
+OAOAOA	else if (_strcmp(args[0], "setenv") == 0)
 		help_setenv();
-	else if (_strcmp(args[0], "unsetenv") == 0)
+OAOAOA	else if (_strcmp(args[0], "unsetenv") == 0)
 		help_unsetenv();
 	else if (_strcmp(args[0], "help") == 0)
 		help_help();
